58a9b70f3ba0a32b76dc7b6c358d8fcd8b5eeb0f
harshsoni21 <harshsoni9119@gmail.com>
2024-07-21 01:06:09 +0530

    keeps changes

---
+++ backend/PrivateExample/private.example.js
@@ -0,0 +1,15 @@
+PORT_ADDRESS=your_port_address
+MONGO_URL=your_mongo_url
+SMTP_HOST=your_smtp_host
+SMTP_EMAIL_PASSWORD=your_smtp_email_password
+SMTP_EMAIL_ADDRESS=your_smtp_email_address
+SMTP_PORT=your_smtp_port
+
+export {
+    PORT_ADDRESS,
+    MONGO_URL,
+    SMTP_HOST,
+    SMTP_EMAIL_ADDRESS,
+    SMTP_EMAIL_PASSWORD,
+    SMTP_PORT
+}
