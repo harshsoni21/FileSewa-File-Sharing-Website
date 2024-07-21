e090fc4dbba79929616c80b52f3ecb80be78070d
harshsoni21 <harshsoni9119@gmail.com>
2024-07-21 22:26:51 +0530

    backend code changes

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use(express.static('public'))
 app.use(express.json());
 
 const corsOption = {
-    origin : ["http://localhost:3000","http://localhost:8080"]
+    origin : ["http://localhost:5173","http://localhost:8080"]
 }
 
 app.use(cors(corsOption));
